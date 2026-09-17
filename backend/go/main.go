729f9323df50aceb7e28202e2ddf08f462cf1092
Janis Hutz <development@janishutz.com>
2026-09-17 12:59:01 +0200

    chore: basic webserver

--- backend/go/main.go
+++ backend/go/main.go
@@ -1,5 +1,13 @@
 package musicplayer
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World at %s", r.URL.Path[1:])
+	})
+	http.ListenAndServe(":8080", nil)
 }
