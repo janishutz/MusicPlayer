93efe3833e48aec20335e8ba0ecad61315397c74
Janis Hutz <development@janishutz.com>
2026-09-17 13:01:09 +0200

    chore: notes

--- backend/go/main.go
+++ backend/go/main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+// TODO:
+// https://github.com/alexedwards/scs for session handling
+// My own SDK for login (that uses OIDC)
+// This should replace the Node.js backend (or maybe have feature parity between the two)
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World at %s", r.URL.Path[1:])
