bf0293d5da32b50441f5f0d0de01e15f74e9f8b6
Janis Hutz <development@janishutz.com>
2026-09-19 19:14:28 +0200

    chore: update schema

--- backend/go/main.go
+++ backend/go/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"time"
 
+	"musicplayer/routes"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/goccy/go-yaml"
 	"github.com/janishutz/oidclogin"
-	"musicplayer/routes"
 )
 
 func main() {
 	r := gin.Default()
+
+	// TODO: First try to load config.secret.yaml, then config.yaml
+
+	// Using https://github.com/goccy/go-yaml for yaml
+	var conf struct {
+	}
+
+	yaml.Unmarshal([]byte(""), &conf)
 
 	// TODO: Security stuff (and auto-config frontend and load config, parse that, etc, etc)
 	r.Use(cors.New(cors.Config{
