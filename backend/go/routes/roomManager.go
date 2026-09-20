0d819974b14dfeac9add95bfeaacaef2577ed2eb
Janis Hutz <development@janishutz.com>
2026-09-20 09:44:05 +0200

    feat: room management basics

--- backend/go/routes/roomManager.go
+++ backend/go/routes/roomManager.go
@@ -1,22 +1,79 @@
 package routes
 
+import (
+	"log"
+	"regexp"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
 type Playlist struct{}
 
 // Represents the current state of the player
 type State struct{}
 
-var rooms map[string]struct {
+type clientList map[*websocket.Conn]bool
+
+type room struct {
 	uid       string
 	state     State
 	playlist  Playlist
 	timestamp int64
-	members []string // TODO: How to best do this?
+	members   clientList
+	lock      sync.RWMutex
 }
 
-func createRoom(name string) {
+var (
+	rooms     map[string]room
+	roomNames map[string]bool
+	regex     *regexp.Regexp
+)
 
+func Init() {
+	r, err := regexp.Compile("[a-zA-Z0-9-]+")
+	if err != nil {
+		log.Fatal("[Rooms] Regex compile failed")
+	}
+
+	regex = r
+	rooms = map[string]room{}
+	roomNames = map[string]bool{}
 }
 
-func closeRoom(name string) {
-	// TODO: Broadcast to all members of this room
+// Createa a new room, with `uid` being the owner and `name` being the name for the room.
+// Returns true if successful, false if room exists already or the room name is invalid
+func createRoom(uid string, name string) bool {
+	if !regex.MatchString(name) {
+		return false
+	}
+
+	if roomNames[name] {
+		return false
+	} else {
+		roomNames[name] = true
+		rooms[name] = room{
+			uid:       uid,
+			state:     State{},
+			playlist:  Playlist{},
+			timestamp: 0,
+			members:   clientList{},
+		}
+
+		return true
+	}
+}
+
+// Close a given room. Returns false if either no such room exists, or not the owner issued the command
+func closeRoom(uid string, name string) bool {
+	if roomNames[name] {
+		if rooms[name].uid != uid {
+			return false
+		}
+		// TODO: Broadcast to all members of this room, then close websockets
+		delete(roomNames, name)
+		delete(rooms, name)
+		return true
+	}
+	return false
 }
