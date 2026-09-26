28ab1d5faa3c63b7067c5949dcee559cc04ef611
Janis Hutz <development@janishutz.com>
2026-09-26 21:06:15 +0200

    feat(web): Overflowing song name scrolls for current song
    will be extracted into separate component soon

--- web/src/components/player/CurrentSong.vue
+++ web/src/components/player/CurrentSong.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+    import {
+        onMounted,
+        ref,
+        useTemplateRef,
+        watch
+    } from 'vue';
     import type {
         Song
     } from '@/ts/dtype/playlist';
@@ -9,6 +15,59 @@
     const props = defineProps<{
         'showAdditionalInfo'?: boolean
     }>();
+    const titleContainer = useTemplateRef( 'title-container' );
+    const title = useTemplateRef( 'title' );
+    const doScroll = ref( false );
+    const SCROLLBACK_DURATION = 500;
+    const WAIT_DURATION = 5000;
+    const MOVE_SPEED = 2;
+
+    let animation: null | Animation = null;
+
+    const updateScrollRule = () => {
+        setTimeout( () => {
+            doScroll.value = ( titleContainer.value?.clientWidth ?? window.innerWidth / 2 ) < ( title.value?.scrollWidth ?? 0 );
+
+            if ( doScroll.value ) {
+                const width = title.value?.scrollWidth ?? 0;
+                const duration = WAIT_DURATION + SCROLLBACK_DURATION + ( width * ( 1 / MOVE_SPEED ) * 10 );
+                const kf = new KeyframeEffect( title.value, [
+                    {
+                        'offset': 0,
+                        'left': '0px'
+                    },
+                    {
+                        'offset': WAIT_DURATION / duration,
+                        'left': '0px'
+                    },
+                    {
+                        'offset': 1 - ( SCROLLBACK_DURATION / duration ),
+                        'left': `-${ width - ( titleContainer.value?.clientWidth ?? 0 ) + 100 }px`
+                    },
+                    {
+                        'offset': 1,
+                        'left': '0px'
+                    }
+                ], {
+                    'delay': 0,
+                    'direction': 'normal',
+                    'duration': duration,
+                    'iterations': Infinity,
+                    'easing': 'cubic-bezier(0.445, 0.05, 0.55, 0.95)'
+                } );
+
+                animation = new Animation( kf, document.timeline );
+                animation.play();
+            } else {
+                if ( animation !== null )
+                    animation?.cancel();
+            }
+        }, 1000 );
+    };
+
+    watch( song, updateScrollRule );
+
+    onMounted( updateScrollRule );
 </script>
 
 <template>
@@ -23,9 +82,11 @@
             <img v-else class="song-cover" src="/logo.jpg">
         </div>
         <div class="song-details">
-            <h1>
-                {{ song?.name ?? 'Not playing' }}
-            </h1>
+            <div ref="title-container" :class="[doScroll ? 'scroll' : undefined]">
+                <h1 ref="title">
+                    {{ song?.name ?? 'Not playing' }}
+                </h1>
+            </div>
             <p class="artist">
                 {{ song?.artist ?? 'No artist' }}
             </p>
@@ -66,12 +127,25 @@
             margin: 10px;
         }
 
-        h1 {
-            width: 100%;
-            font-size: 2.5rem;
-            text-wrap: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
+        >div {
+            height: 4rem;
+            h1 {
+                font-size: 2.5rem;
+                text-wrap: nowrap;
+                margin: 0;
+            }
+
+            &.scroll {
+                width: 100%;
+                position: relative;
+
+                h1 {
+                    position: absolute;
+                    left: 0;
+                    // animation: cubic-bezier(0.445, 0.05, 0.55, 0.95);
+                    // animation: slide ease 20s infinite;
+                }
+            }
         }
 
         .artist {
@@ -89,10 +163,16 @@
 
 @keyframes slide {
     0% {
-        left: 0;
+        transform: translateX(0);
+    }
+    40% {
+        transform: translateX(0);
+    }
+    95% {
+        transform: translateX(-100%);
     }
     100% {
-        left: 100%;
+        transform: translateX(0);
     }
 }
 </style>
